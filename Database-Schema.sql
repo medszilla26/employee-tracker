fbed0543db5b1e4ffebb9c0c1be158cc1f8f38e1
M3D5 <pablohdzjr@gmail.com>
2020-07-02 14:56:51 -0500

    Started DB in VS Code

---
+++ Database-Schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE database employee_db;
+
+USE employee_db;
+
+CREATE TABLE Department (
+ 
+);
+
+CREATE TABLE Employee Roll (
+
+  
+);
+
+CREATE TABLE Employee (
+  
+
+);
+
+
+
+
+
+
+
+
+
+
+
+
