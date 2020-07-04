54ddaa5d9982a33a40af84862fa42edcb7ce5386
M3D5 <pablohdzjr@gmail.com>
2020-07-03 21:09:50 -0500

    added prompt choices based on gif

--- employeedb.js
+++ employeedb.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql2");
+const { start } = require("repl");
 var inquirer = require(inquirer);
 
 var connection = mysql.createConnection({
@@ -8,3 +9,19 @@ var connection = mysql.createConnection({
   password: "",
   database: "employee_db",
 });
+
+connection.connect(function (err) {
+  if (err) throw err;
+  start();
+});
+
+function start() {
+  inquirer.prompt({
+    name: "options",
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+      "View All Employees, View All Employees by Manager, Add Employee, Remove Employee, Update Employee Role, Update Employee Manager, View All Roles",
+    ],
+  });
+}
