6fdb0852d8beaa66ad9930dee4eeb182f996b07d
M3D5 <pablohdzjr@gmail.com>
2020-07-04 22:18:50 -0500

    started function switch cases

--- employeedb.js
+++ employeedb.js
@@ -17,21 +17,50 @@ connection.connect((err) => {
 });
 
 function startPrompt() {
-  inquirer.prompt({
-    name: "options",
-    type: "list",
-    message: "What would you like to do?",
-    choices: [
-      "View All Employees",
-      "View All Employees by Manager",
-      "Add Employee",
-      "Remove Employee",
-      "Update Employee Role",
-      "Update Employee Manager",
-      "View All Roles",
-    ],
-  });
-  //   .then(function (answer) {
-  //       if (answer.options === "View All Employees")
-  //   })
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Employees by Manager",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee Role",
+        "Update Employee Manager",
+        "View All Roles",
+      ],
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+        case "View All Employees":
+          employeeView();
+          break;
+
+        case "View All Employees by Manager":
+          viewByManager();
+          break;
+
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "Remove Employee":
+          removeEmployee();
+          break;
+
+        case "Update Employee Role":
+          updateRole();
+          break;
+
+        case "Update Employee Manager":
+          updateManager();
+          break;
+
+        case "View All Roles":
+          viewAllRoles();
+          break;
+      }
+    });
 }
