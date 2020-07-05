c33c5d855b2e51c97e5d4d8a142046527f084279
M3D5 <pablohdzjr@gmail.com>
2020-07-04 22:06:55 -0500

    successfully started to run prompt questions

--- employeedb.js
+++ employeedb.js
@@ -13,20 +13,25 @@ var connection = mysql.createConnection({
 connection.connect((err) => {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  connection.end();
-
-  //start();
+  startPrompt();
 });
-// function start() {
-//   inquirer.prompt({
-//     name: "options",
-//     type: "list",
-//     message: "What would you like to do?",
-//     choices: [
-//       "View All Employees", "View All Employees by Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "View All Roles",
-//     ],
-//   });
-//   .then(function (answer) {
-//       if (answer.options === "View All Employees")
-//   })
-// }
+
+function startPrompt() {
+  inquirer.prompt({
+    name: "options",
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+      "View All Employees",
+      "View All Employees by Manager",
+      "Add Employee",
+      "Remove Employee",
+      "Update Employee Role",
+      "Update Employee Manager",
+      "View All Roles",
+    ],
+  });
+  //   .then(function (answer) {
+  //       if (answer.options === "View All Employees")
+  //   })
+}
